4b8348c4f0cf92189bad3d63203b5eecadfe9a00
alloza95 <naldusbros@gmail.com>
2020-05-26 18:14:16 +0200

    implement box in poster

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -12,7 +12,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TMDB Api'),
+        title: Text('TMDB Apii'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search, color: Colors.white),
